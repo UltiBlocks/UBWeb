5c9b82d91b5f2135a550c008f55ea9c13dad07fc
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-02-09 15:16:11 -0600

    Update 404.php
    Little concept page for you

--- src/404.php
+++ src/404.php
@@ -4,7 +4,11 @@
 <main>
 <div style="flex: 1;">
 <div style="text-align: center; margin-top: auto;">
-<h1>404. Whoops!</h1>
+  <img src="/images/svg/cgIcons
+/compatibility.svg" type="svg"></img>
+<h1>404.</h1>
+  We hit a road block, wait, road blocks don't exist!
+  Whoops, we mean, we hit a roadblock.
 <a href="/ultiblocks">Go back to the homepage</a>
 </div>
 </div>
