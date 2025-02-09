582d383af9e8c4120fa4923e5244b840e98fe127
AmpElectrecuted <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-09 21:35:29 +0000

    Update 404.php

--- src/404.php
+++ src/404.php
@@ -1,14 +1,12 @@
 <?php http_response_code(404); ?>
 <?php include 'header.php'; ?>
-
 <main>
 <div style="flex: 1;">
 <div style="text-align: center; margin-top: auto;">
-  <img src="/images/svg/cgIcons
-/compatibility.svg" type="svg"></img>
+<img src="/images/svg/cgIcons/compatibility.svg" type="svg"></img>
 <h1>404.</h1>
-  We hit a road block, wait, road blocks don't exist!
-  Whoops, we mean, we hit a roadblock.
+We hit a road block, wait, road blocks don't exist!
+Whoops, we mean, we hit a roadblock.
 <a href="/ultiblocks">Go back to the homepage</a>
 </div>
 </div>
