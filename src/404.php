a1826e0b36f3147a5e8653a7d38e172b9c0b59cd
AmpElectrecuted <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-09 21:59:07 +0000

    oops

--- src/404.php
+++ src/404.php
@@ -5,7 +5,7 @@
 <div style="text-align: center; margin-top: auto;">
 <img src="/images/svg/cgIcons/compatibility.svg" type="svg"></img>
 <h1>404.</h1>
-<p>>We hit a roadblock!</p>
+<p>We hit a roadblock!</p>
 <p><a href="/ultiblocks">Go back to the homepage</a></p>
 </div>
 </div>
