4e56717d9bf1d2e35419b5b5c4509103118cdeeb
AmpElectrecuted <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-09 21:58:34 +0000

    stuff

--- src/404.php
+++ src/404.php
@@ -5,9 +5,8 @@
 <div style="text-align: center; margin-top: auto;">
 <img src="/images/svg/cgIcons/compatibility.svg" type="svg"></img>
 <h1>404.</h1>
-We hit a road block, wait, road blocks don't exist!
-Whoops, we mean, we hit a roadblock.
-<a href="/ultiblocks">Go back to the homepage</a>
+<p>>We hit a roadblock!</p>
+<p><a href="/ultiblocks">Go back to the homepage</a></p>
 </div>
 </div>
 </main>
