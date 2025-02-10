7105b15a2ace8d575d75fb828ea966bc03d003ae
AmpElectrecuted <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-10 08:45:59 +0000

    Update and rename help.html to help.php

---
+++ help.php
@@ -0,0 +1,6 @@
+<?php include "header.php">
+<title>Help | UltiBlocks</title>
+Need help?
+Contact us at our GitHub Repo,<a src="https://github.com/UltiBlocks/UBWeb/issues"> here.</a>
+Also see our forum topic <a src="https://scratch.mit.edu/discuss/topic/804448">here</a>.
+<?php include "footer.php">
