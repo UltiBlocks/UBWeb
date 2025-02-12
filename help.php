06d70021589120f93b30d87ab8d7ae200718444c
GvYoutube <119984713+Gvyt@users.noreply.github.com>
2025-02-12 14:01:04 -0600

    Update help.php

--- help.php
+++ help.php
@@ -1,6 +1,8 @@
 <?php include "header.php">
-<title>Help | UltiBlocks</title>
+<title>Help | AmpMod</title>
 Need help?
 Contact us at our GitHub Repo,<a src="https://github.com/UltiBlocks/UBWeb/issues"> here.</a>
 Also see our forum topic <a src="https://scratch.mit.edu/discuss/topic/804448">here</a>.
+  You can also checkout this video below.
+  <iframe src="placeholder.com/place.holder"></iframe>
 <?php include "footer.php">
