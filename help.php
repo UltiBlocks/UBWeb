4bd865cbb047d5638e0e77bea9f0e86865bae025
amp <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-12 22:02:21 +0000

    Update help.php

--- help.php
+++ help.php
@@ -1,5 +1,5 @@
 <?php include "header.php">
-<title>Help | AmpMod</title>
+<title>Help | UltiBlocks</title>
 Need help?
 Contact us at our GitHub Repo,<a src="https://github.com/UltiBlocks/UBWeb/issues"> here.</a>
 Also see our forum topic <a src="https://scratch.mit.edu/discuss/topic/804448">here</a>.
