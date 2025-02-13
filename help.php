96c0063c31a7f7e6c5ae1ae135fc5e6cad979b25
amp <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-13 19:20:00 +0000

    Update help.php

--- help.php
+++ help.php
@@ -1,8 +1,4 @@
 <?php include "header.php">
 <title>Help | UltiBlocks</title>
-Need help?
-Contact us at our GitHub Repo,<a src="https://github.com/UltiBlocks/UBWeb/issues"> here.</a>
-Also see our forum topic <a src="https://scratch.mit.edu/discuss/topic/804448">here</a>.
-  You can also checkout this video below.
-  <iframe src="placeholder.com/place.holder"></iframe>
-<?php include "footer.php">
+Need help? Ask on our <a src="https://scratch.mit.edu/discuss/topic/804448">forum topic</a>.
+  <?php include "footer.php">
